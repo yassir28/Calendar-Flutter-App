53e73e9380b0169655b9ee0e1934b818c93ca9d1
yassir28 <yassirmakloul@gmail.com>
2021-02-26 23:52:45 +0100

    models

---
+++ lib/models/user.dart
@@ -0,0 +1,10 @@
+class User {
+  final String uid;
+  final int periodLength;
+  final int periodCycle;
+  final int periodDate;
+
+  User({this.uid, this.periodCycle, this.periodLength, this.periodDate});
+}
+
+//these are our users infos.
